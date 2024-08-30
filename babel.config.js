97675009cc34a616884950de2d44609b3151746c
Lucas Santos <lucassm02@gmail.com>
2024-08-30 17:57:05 -0300

    chore: add project structure

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    '@babel/plugin-transform-typescript',
+    '@babel/plugin-transform-modules-commonjs',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
+};
