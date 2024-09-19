c036f80e996e3e8eb0c851336e5bcbb12db6a236
Lucas Santos <lucassm02@gmail.com>
2024-09-19 10:16:44 -0300

    refactor: update the query source regarding the logged-in user's data

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,10 +1,14 @@
+import { userInfo } from 'os';
+
 import { generateUniquePassword } from '@/util/generate-unique-password';
+
+const { homedir, shell } = userInfo();
 
 export default {
   ENCRYPTION_PASSWORD: generateUniquePassword(),
   USER: {
-    HOME: process.env.HOME ?? '/root',
-    SHELL: process.env.SHELL ?? null,
+    HOME: homedir,
+    SHELL: shell,
     GHTOOLS_EDITOR: process.env.GHTOOLS_EDITOR ?? null
   }
 };
